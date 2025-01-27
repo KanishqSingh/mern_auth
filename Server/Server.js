19651661293a0a3b3531fa3bb13a449cdf5640fb
Kanishq SIngh <singhkanishq06@gmail.com>
2025-01-27 22:42:37 +0530

    Third commit

--- Server/Server.js
+++ Server/Server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import cookieParser from 'cookie-parser'
 import connectDB from './config/Mongodb.js'
+import authRouter from './Routes/authRoutes.js'
 
 
 const app = express() 
@@ -15,7 +16,9 @@ app.use(cookieParser())
 app.use(cors({credentials: true})) // to send cookies as res 
 
 
-
+//API endpoints
 app.get('/',(req,res) => res.send("API  Working"))
+app.use('/api/auth',authRouter)
+
 
 app.listen(port,()=>console.log(`Server started on PORT : ${port}`))
--- Server/controller/authController.js
+++ Server/controller/authController.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
-import userModel from '../models/userModel';
+import userModel from '../models/userModel.js';
 
 
 //register user
@@ -27,7 +27,7 @@ export const register = async(req,res) =>{
         res.cookie('token',token,{
             httpOnly:true,
             secure:process.env.NODE_ENV === 'production',
-            sameSite:process.env.NODE-ENV === 'production' ? 'none' : 'strict',
+            sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge:7 * 24 * 60 * 60 * 1000,
 
         });
@@ -65,7 +65,7 @@ export const login = async (req,res) =>{
         res.cookie('token',token,{
             httpOnly:true,
             secure:process.env.NODE_ENV === 'production',
-            sameSite:process.env.NODE-ENV === 'production' ? 'none' : 'strict',
+            sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge:7 * 24 * 60 * 60 * 1000,
 
         });
@@ -87,7 +87,7 @@ export const logout = async(req,res)=>{
         res.clearCookie('token', {
             httpOnly:true,
             secure:process.env.NODE_ENV === 'production',
-            sameSite:process.env.NODE-ENV === 'production' ? 'none' : 'strict',
+            sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             
 
         });
